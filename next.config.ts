c5acbb636de22702c8e239281f346c0cf96df63f
Hùng Nguyễn <hungnguyen@Hungs-MacBook-Pro.local>
2025-05-20 21:48:05 +0700

    t

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    typescript: {
+        ignoreBuildErrors: false, // Recommended
+    },
 };
 
 export default nextConfig;
